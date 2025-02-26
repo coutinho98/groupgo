0c26c19979d608b8983ad92c911c21b2f312e9ff
coutinho98 <mateuscouto98@outlook.com>
2025-02-26 10:13:28 -0300

    mexi na borda da foto de perfil

--- src/components/Profile/ProfileCard.jsx
+++ src/components/Profile/ProfileCard.jsx
@@ -38,11 +38,11 @@ const ProfileCard = ({ firstName, lastName, location }) => {
     return (
         <Section title="My Profile" fontSize="text-xl" onClick={handleEditClick}>
             <div className="flex items-start gap-6 mb-8 py-4">
-                <div className="w-20 h-20 bg-purple-500 rounded-full overflow-hidden flex-shrink-0">
+                <div className="w-20 h-20 bg-purple-500 rounded-full overflow-hidden flex-shrink-0 border-4 border-teal-600">
                     <img
                         src={imageUrl || 'https://pt.vecteezy.com/arte-vetorial/7226475-user-account-circle-glyph-color-icon-user-profile-picture-userpic-silhuette-symbol-on-white-background-with-no-outline-negative-space-vector-illustration'}
                         alt={`${firstName} ${lastName}`}
-                        className="w-full h-full object-cover"
+                        className="w-full h-full object-cover "
                     />
                     <input
                         type='file'
