0ec9c0e55cf9f4002b10ea8066682f2d335ab99c
coutinho98 <mateuscouto98@outlook.com>
2025-03-04 20:36:26 -0300

    back button on forgotpassword screen added

--- src/pages/ForgotPassword/ForgotPassword.jsx
+++ src/pages/ForgotPassword/ForgotPassword.jsx
@@ -3,12 +3,16 @@ import Card from '../../components/LoginPage/Card'
 import Header from '../../components/LoginPage/Header'
 import Input from '../../components/Input'
 import Button from "../../components/Button"
+import { LeftArrow } from "../../components/icons/Icons"
+import { Link } from "react-router"
 
 
 const ForgotPassword = () => {
     return (
         <Layout>
             <Card>
+                <button className="-ml-1 text-teal-700"> <Link to="/"><LeftArrow />
+                </Link></button>
                 <Header
                     title="Forgot Password?"
                     subtitle="enter the email or @username to change your password"
@@ -18,8 +22,6 @@ const ForgotPassword = () => {
                     id="text"
                     type="text"
                     name="text"
-                    /*  value={recovery}
-                     onChange={handleChange} */
                     placeholder='email or @username'
                     required
                 ></Input>
