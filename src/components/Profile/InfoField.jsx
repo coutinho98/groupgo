80bc215f395a2674fc8cf2410b1e8f35d02ed148
coutinho98 <mateuscouto98@outlook.com>
2025-03-10 08:09:48 -0300

    feat: created in the InfoField component a future adaptation to edit the profile, will it work? idk!

--- src/components/Profile/InfoField.jsx
+++ src/components/Profile/InfoField.jsx
@@ -1,8 +1,18 @@
-const InfoField = ({ label, value, spanFull }) => {
+const InfoField = ({ label, value, spanFull, name, isEditing, editedValue, onChange }) => {
     return (
         <div className={spanFull ? "col-span-2" : ""}>
             <label className="block text-sm text-gray-500 mb-1">{label}</label>
-            <div className="truncate text-black font-semibold">{value}</div>
+            {isEditing ? (
+                <input
+                    type="text"
+                    name={name}
+                    value={editedValue || value}
+                    onChange={onChange}
+                    className="w-full p-2 border border-gray-300 rounded text-black"
+                />
+            ) : (
+                <div className="truncate text-black font-semibold">{value}</div>
+            )}
         </div>
     )
 }
--- src/components/Profile/Section.jsx
+++ src/components/Profile/Section.jsx
@@ -1,11 +1,8 @@
-import EditButton from "./EditButton"
-
 const Section = ({ title, children, onClick, fontSize = 'font-medium' }) => {
     return (
-        <div className="mb-5 rounded-lg shadow-md p-6">
+        <div className="mb-5  p-6">
             <div className="flex justify-between items-center mb-4">
                 <h3 className={`${fontSize}`}>{title}</h3>
-                <EditButton onClick={onClick} />
             </div>
             {children}
         </div>
